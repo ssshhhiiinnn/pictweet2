6d3f44061ac0a2260193b9d29a01099b2a5db1ab
ssshhhiiinnn <shin.3534.a@gmail.com>
2017-10-10 23:43:54 +0900

    ツイート一覧にnicknameが表示されるように仕様変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,5 @@
 class UsersController < ApplicationController
   def show
-    @user_id = current_user.id
     @nickname = current_user.nickname
     @tweets = current_user.tweets.page(params[:page]).per(5).order("created_at DESC")
   end
