a69a846e59630750e5b9180240742ea90bd84040
ssshhhiiinnn <shin.3534.a@gmail.com>
2017-10-11 18:26:40 +0900

    【tweetコメント機能】 ルーティングを行った

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,27 @@
+# Rails.application.routes.draw do
+#   devise_for :users
+#   root  'tweets#index'
+#   get   'tweets'         => 'tweets#index'
+#   get   'tweets/new'     => 'tweets#new'
+#   post  'tweets'         => 'tweets#create'
+#   get   'users/:id'      => 'users#show'
+#   get  'tweets/:id'      => 'tweets#show'
+# end
+
+# resourceメソッドを使った
+# Rails.application.routes.draw do
+#   devise_for :users
+#   root 'tweets#index'
+#   resources :tweets                     #tweets_controllerに対してのresourcesメソッド
+#   resources :users, only: [:show]       #users_controllerに対してのresourcesメソッド
+# end
+
+# ネストを使った
 Rails.application.routes.draw do
   devise_for :users
-  root  'tweets#index'
-  get   'tweets'         => 'tweets#index'
-  get   'tweets/new'     => 'tweets#new'
-  post  'tweets'         => 'tweets#create'
-  get   'users/:id'      => 'users#show'
-  get  'tweets/:id'      => 'tweets#show'
+  root 'tweets#index'
+  resources :tweets do
+    resources :comments, only: [:create]
+  end
+  resources :users, only: [:show]
 end
