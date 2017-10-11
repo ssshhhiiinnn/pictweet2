d9adb197edc63e91bdb4ee6a8893c8c011934ae1
ssshhhiiinnn <shin.3534.a@gmail.com>
2017-10-11 20:08:16 +0900

    【tweetコメント機能】 Viewの変更　実装完成

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -21,6 +21,7 @@ class TweetsController < ApplicationController
 
   def show
     @tweet = Tweet.find(params[:id])
+    @comments = @tweet.comments.includes(:user)
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 # Rails.application.routes.draw do
-#   devise_for :users
-#   root  'tweets#index'
-#   get   'tweets'         => 'tweets#index'
-#   get   'tweets/new'     => 'tweets#new'
-#   post  'tweets'         => 'tweets#create'
-#   get   'users/:id'      => 'users#show'
-#   get  'tweets/:id'      => 'tweets#show'
+#devise_for :users
+# root 'tweets#index'
+# get   'tweets'      =>  'tweets#index'
+# get 'tweets/new' => 'tweets#new'
+# post 'tweets' => 'tweets#create'
+# get 'users/:id' => 'users#show'
+# delete 'tweets/:id' => 'tweets#destroy'
+# get 'tweets/:id/edit' => 'tweets#edit'
+# patch 'tweets/:id'  => 'tweets#update'
+# get 'tweets/:id' => 'tweets#show'
 # end
 
 # resourceメソッドを使った
