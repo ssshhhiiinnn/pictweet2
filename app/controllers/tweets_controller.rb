de39e55a02ed970458abf38e85c21f43791f6be5
ssshhhiiinnn <shin.3534.a@gmail.com>
2017-10-11 14:42:49 +0900

    削除機能の実装

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -13,6 +13,11 @@ class TweetsController < ApplicationController
      Tweet.create(image: tweet_params[:image], text: tweet_params[:text], user_id: current_user.id)
   end
 
+  def destroy
+    tweet = Tweet.find(params[:id])
+    tweet.destroy if tweet.user_id == current_user.id
+  end
+
   private
   def tweet_params
     params.permit(:image, :text)
@@ -21,5 +26,4 @@ class TweetsController < ApplicationController
   def move_to_index
     redirect_to action: :index unless user_signed_in?
   end
-
 end
